8c9708bc4bd5ee3576aac3a02de0a7dac1dcdbd4
amberlislaya <laya.playapps@gmail.com>
2023-04-05 21:32:05 -0300

    subiendo-proyecto-rickandmorty2

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Cards.module.scss';
+import styles from './Cards.module.css';
 
 const Cards = ({ results, page }) => {
 let display;
